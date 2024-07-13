8470c9bd68803e612e6ef4ff484d312740330a9a
Mitchell Abbott <mitchell-abbott@users.noreply.github.com>
2024-07-13 16:19:11 -0500

    Add cache headers, loader and time fetch

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,39 @@
-import type { MetaFunction } from "@remix-run/node";
+import {json, MetaFunction} from "@remix-run/node";
+import {HeadersFunction} from "@vercel/remix";
+import {useLoaderData} from "@remix-run/react";
+import {fetch} from "@remix-run/web-fetch";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: " Remix Cache Test" },
+    { name: "description", content: "Remix Cache Test!" },
   ];
 };
 
+export const headers: HeadersFunction = () => ({
+  'Cache-Control': 's-maxage=1, stale-while-revalidate=299',
+});
+
+function getCurrentTime() {
+  return fetch('http://worldtimeapi.org/api/timezone/America/Chicago')
+      .then( (response) => response.json() as Promise<{datetime: string}>);
+}
+
+export async function loader() {
+  const {datetime} = await getCurrentTime();
+  return json({
+    title: 'Remix Cache Test',
+    datetime
+  });
+}
+
 export default function Index() {
+  const { title, datetime } = useLoaderData<typeof loader>();
+  const currentTime = new Date(datetime);
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1 className="text-3xl">{title}</h1>
+      <h1 className="text-2xl">Current Time: {currentTime.toUTCString()}</h1>
     </div>
   );
 }
